0fd1c6440074a3309e95a4629cc90904012c387c
Starath <164960528+Starath@users.noreply.github.com>
2025-05-20 21:50:29 +0700

    refactor: implement multistep movement

--- src/controller/solver/RushHourSolver.java
+++ src/controller/solver/RushHourSolver.java
@@ -1,4 +1,3 @@
-// In src/controller/solver/RushHourSolver.java
 package controller.solver;
 
 import controller.SolutionResult;
@@ -7,75 +6,70 @@ import java.util.List;
 import java.util.function.Consumer;
 import model.GameState;
 import model.core.Board;
-import model.core.Move; 
+import model.core.Move;
 
 public abstract class RushHourSolver {
-    protected GameState initialState;
-    protected int visitedNodesCount;
-    protected long executionTime;
+	protected GameState initialState;
+	protected int visitedNodesCount;
+	protected long executionTime;
 
-    // --- AWAL Perubahan untuk Progress Reporting (FIXED) ---
-    // Menggunakan Consumer untuk callback publikasi progres.
-    // Consumer akan menerima Integer (visitedNodesCount)
-    protected Consumer<Integer> progressEventConsumer;
+	protected Consumer<Integer> progressEventConsumer;
 
-    /**
-     * Mengatur callback (Consumer) yang akan dipanggil untuk mempublikasikan progres.
-     * @param consumer Consumer yang akan menerima update progres (misalnya, jumlah node yang dikunjungi).
-     */
-    public void setProgressEventConsumer(Consumer<Integer> consumer) {
-        this.progressEventConsumer = consumer;
-    }
+	public void setProgressEventConsumer(Consumer<Integer> consumer) {
+		this.progressEventConsumer = consumer;
+	}
 
-    /**
-     * Metode helper untuk solver konkret mempublikasikan progres.
-     * Ini akan memanggil Consumer yang telah diatur, yang kemudian akan memanggil SwingWorker.publish().
-     */
-    protected void publishVisitedNodesCount() {
-        if (this.progressEventConsumer != null) {
-            this.progressEventConsumer.accept(this.visitedNodesCount);
-        }
-    }
+	protected void publishVisitedNodesCount() {
+		if (this.progressEventConsumer != null) {
+			this.progressEventConsumer.accept(this.visitedNodesCount);
+		}
+	}
 
-    public RushHourSolver(GameState initialState) {
-        this.initialState = initialState;
-        this.visitedNodesCount = 0;
-        this.executionTime = 0;
-    }
+	public RushHourSolver(GameState initialState) {
+		this.initialState = initialState;
+		this.visitedNodesCount = 0;
+		this.executionTime = 0;
+	}
 
-    public int getVisitedNodesCount() {
-        return this.visitedNodesCount;
-    }
+	public int getVisitedNodesCount() {
+		return this.visitedNodesCount;
+	}
 
-    public long getExecutionTime() {
-        return this.executionTime;
-    }
+	public long getExecutionTime() {
+		return this.executionTime;
+	}
 
 	public List<Board> reconstructBoardStates(GameState finalState) {
 		List<Board> boardStatesPath = new ArrayList<>();
 		List<Move> moves = finalState.getMoves();
-		
+
 		if (this.initialState == null || this.initialState.getBoard() == null) {
-			 System.err.println("Solver Error: Initial state or board is null during board state reconstruction.");
-			return boardStatesPath; 
+			 System.err.println("Solver Error: Initial state atau initial board adalah null saat rekonstruksi board state.");
+			return boardStatesPath;
 		}
 
-		Board currentIterationBoard = this.initialState.getBoard().clone(); 
+		Board currentIterationBoard = this.initialState.getBoard().clone();
 
 		for (Move move : moves) {
-			Board nextBoardState = currentIterationBoard.clone(); 
-			boolean success = nextBoardState.movePiece(move.getPieceId(), move.getDirection());
+			Board nextBoardState = currentIterationBoard.clone();
+
+			boolean success = nextBoardState.movePiece(move.getPieceId(), move.getDirection(), move.getSteps());
+
 			if(success) {
 				boardStatesPath.add(nextBoardState);
-				currentIterationBoard = nextBoardState; 
+				currentIterationBoard = nextBoardState;
 			} else {
-				 System.err.println("Solver Warning: Failed to apply move during board state reconstruction. Move: " + move.getPieceId() + " " + move.getDirection());
-				 boardStatesPath.add(currentIterationBoard.clone()); 
+				 System.err.println("Solver Warning: Gagal menerapkan move saat rekonstruksi board state. Move: " +
+								   move.getPieceId() + " ke " + move.getDirection() +
+								   " sebanyak " + move.getSteps() + " langkah.");
+				
+				
+				 boardStatesPath.add(currentIterationBoard.clone());
 			}
 		}
 		return boardStatesPath;
 	}
-    
-    public abstract SolutionResult solve();
-    public abstract String getName();
+
+	public abstract SolutionResult solve();
+	public abstract String getName();
 }
--- src/model/core/Board.java
+++ src/model/core/Board.java
@@ -2,208 +2,313 @@ package model.core;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List; 
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 public class Board {
-    private final int width;
-    private final int height;
-    private final ArrayList<Piece> pieces;
-    private final Position exitPosition;
-    private Piece primaryPiece;
-    private final Piece[][] grid;
+	private final int width;
+	private final int height;
+	private final ArrayList<Piece> pieces;
+	private final Position exitPosition;
+	private Piece primaryPiece;
+	private final Piece[][] grid;
 
-    public Board(int width, int height, ArrayList<Piece> pieces, Position exitPosition) {
-        this.width = width;
-        this.height = height;
-        this.exitPosition = new Position(exitPosition.getX(), exitPosition.getY());
-        
-        this.pieces = new ArrayList<>();
-        for (Piece p : pieces) {
-            Piece pieceCopy = p.clone();
-            this.pieces.add(pieceCopy);
-            
-            if (pieceCopy.isPrimary()) {
-                this.primaryPiece = pieceCopy;
-            }
-        }
-        this.grid = new Piece[this.height][this.width];
-        // Panggil metode helper untuk mengisi grid berdasarkan daftar pieces yang sudah di-clone
-        populateGridFromPieces();
-    }
+	public Board(int width, int height, ArrayList<Piece> pieces, Position exitPosition) {
+		this.width = width;
+		this.height = height;
+		this.exitPosition = new Position(exitPosition.getX(), exitPosition.getY());
 
-    private void populateGridFromPieces() {
-        // Grid diinisialisasi dengan null secara default untuk sel kosong
-        for (Piece piece : this.pieces) {
-            if (piece.getPositions() != null) {
-                for (Position pos : piece.getPositions()) {
-                    // Pastikan posisi valid sebelum mengakses grid
-                    if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
-                        this.grid[pos.getY()][pos.getX()] = piece;
-                    } else {
-                        // Tambahkan penanganan error atau logging jika posisi bidak di luar batas papan
-                        // Seharusnya ini tidak terjadi jika input file dan logika Piece valid
-                        System.err.println("Peringatan: Posisi bidak " + piece.getId() + " di (" + pos.getX() + "," + pos.getY() + ") berada di luar batas papan saat mengisi grid.");
-                    }
-                }
-            }
-        }
-    }
+		this.pieces = new ArrayList<>();
+		for (Piece p : pieces) {
+			Piece pieceCopy = p.clone();
+			this.pieces.add(pieceCopy);
 
-    public boolean movePiece(char pieceId, Direction direction) {
-        Piece pieceToMove = this.getPieceById(pieceId);
+			if (pieceCopy.isPrimary()) {
+				this.primaryPiece = pieceCopy;
+			}
+		}
+		this.grid = new Piece[this.height][this.width];
+		populateGridFromPieces();
+	}
 
-        if (pieceToMove == null) {
-            System.err.println("Error: Bidak dengan ID '" + pieceId + "' tidak ditemukan di papan untuk dipindahkan.");
-            return false;
-        }
+	private void populateGridFromPieces() {
+		for (int r = 0; r < this.height; r++) {
+			for (int c = 0; c < this.width; c++) {
+				this.grid[r][c] = null;
+			}
+		}
+		for (Piece piece : this.pieces) {
+			if (piece.getPositions() != null) {
+				for (Position pos : piece.getPositions()) {
+					if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
+						this.grid[pos.getY()][pos.getX()] = piece;
+					} else {
+						System.err.println("Peringatan: Posisi bidak " + piece.getId() + " di (" + pos.getX() + "," + pos.getY() + ") berada di luar batas papan saat mengisi grid.");
+					}
+				}
+			}
+		}
+	}
 
-        
-        if (!pieceToMove.canMove(direction, this)) {
-            return false;
-        }
+	public boolean movePiece(char pieceId, Direction direction, int steps) {
+		if (steps < 1) {
+			System.err.println("Error: Jumlah langkah tidak valid (" + steps + ") untuk movePiece.");
+			return false;
+		}
 
-        List<Position> oldPositions = pieceToMove.getPositions(); // Ambil posisi saat ini
-        for (Position pos : oldPositions) {
-            if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
-                this.grid[pos.getY()][pos.getX()] = null;
-            }
-        }
+		Piece pieceToMove = this.getPieceById(pieceId);
 
-        boolean moveSuccessful = pieceToMove.move(direction, this);
+		if (pieceToMove == null) {
+			System.err.println("Error: Bidak dengan ID '" + pieceId + "' tidak ditemukan di papan untuk dipindahkan.");
+			return false;
+		}
 
-        if (moveSuccessful) {
-            List<Position> newPositions = pieceToMove.getPositions(); // Ambil posisi yang sudah diperbarui
-            for (Position pos : newPositions) {
-                if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
-                    this.grid[pos.getY()][pos.getX()] = pieceToMove;
-                } else {
-                    System.err.println("Peringatan: Posisi baru bidak " + pieceToMove.getId() + " di (" + pos.getX() + "," + pos.getY() + ") berada di luar batas papan setelah bergerak.");
-                }
-            }
-            return true;
-        } else {
-            for (Position pos : oldPositions) {
-                if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
-                    this.grid[pos.getY()][pos.getX()] = pieceToMove;
-                }
-            }
-            return false;
-        }
-    }
+		List<Position> currentPiecePositions = pieceToMove.getPositions();
+		for (Position pos : currentPiecePositions) {
+			if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
+				this.grid[pos.getY()][pos.getX()] = null;
+			}
+		}
 
-    public boolean isOccupied(Position position) {
-        int x = position.getX();
-        int y = position.getY();
+		pieceToMove.moveBySteps(direction, steps);
 
-        if (y >= 0 && y < this.height && x >= 0 && x < this.width) {
-            return this.grid[y][x] != null;
-        }
-        return false; 
-    }
+		List<Position> newPiecePositions = pieceToMove.getPositions();
+		for (Position pos : newPiecePositions) {
+			if (pos.getY() >= 0 && pos.getY() < this.height && pos.getX() >= 0 && pos.getX() < this.width) {
+				if (this.grid[pos.getY()][pos.getX()] != null && this.grid[pos.getY()][pos.getX()] != pieceToMove) {
+					 System.err.println("Error Kritis: Tabrakan terdeteksi saat memindahkan piece " + pieceId + " ke ("+pos.getX()+","+pos.getY()+") yang sudah ditempati oleh " + this.grid[pos.getY()][pos.getX()].getId() +". Rollback tidak diimplementasikan di sini.");
+					
+					 return false;
+				}
+				this.grid[pos.getY()][pos.getX()] = pieceToMove;
+			} else {
+				System.err.println("Peringatan Kritis: Posisi baru bidak " + pieceToMove.getId() + " di (" + pos.getX() + "," + pos.getY() + ") berada di luar batas papan SETELAH bergerak. Ini mengindikasikan masalah pada validasi sebelumnya.");
+				 for (Position oldPos : currentPiecePositions) {
+					if (oldPos.getY() >= 0 && oldPos.getY() < this.height && oldPos.getX() >= 0 && oldPos.getX() < this.width) {
+						 this.grid[oldPos.getY()][oldPos.getX()] = pieceToMove;
+					}
+				}
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public boolean isPrimaryPieceAtExit() {
-        if (primaryPiece == null) return false;
-        for (Position p : primaryPiece.getPositions()) {
-            boolean atExit = (p.getX() == exitPosition.getX() &&
-                             p.getY() == exitPosition.getY() + 1) ||
-                             (p.getX() == exitPosition.getX() + 1 &&
-                             p.getY() == exitPosition.getY()) ||
-                             (p.getX() == exitPosition.getX() &&
-                             p.getY() == exitPosition.getY() - 1) ||
-                             (p.getX() == exitPosition.getX() - 1 &&
-                             p.getY() == exitPosition.getY());
-            if (atExit) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean isOccupied(Position position) {
+		int x = position.getX();
+		int y = position.getY();
 
-    public ArrayList<Move> getValidMoves() {
-        ArrayList<Move> moves = new ArrayList<>();
-        for (Piece p : pieces) {
-            for (Direction d : Direction.values()) {
-                if (p.canMove(d, this)) {
-                    moves.add(new Move(p.getId(), d));
-                }
-            }
-        }
-        return moves;
-    }
+		if (y >= 0 && y < this.height && x >= 0 && x < this.width) {
+			return this.grid[y][x] != null;
+		}
+		return true;
+	}
 
-    public Board clone() {
-        ArrayList<Piece> piecesCopy = new ArrayList<>();
-        for (Piece p : this.pieces) {
-            piecesCopy.add(p.clone());
-        }
-        
-        Position exitPositionCopy = new Position(this.exitPosition.getX(), this.exitPosition.getY());
-        
-        return new Board(this.width, this.height, piecesCopy, exitPositionCopy);
-    }
+	public boolean isPrimaryPieceAtExit() {
+		if (primaryPiece == null) return false;
 
-    public List<Piece> getPieces() {
-        return this.pieces;
-    }
 
-    public Piece getPrimaryPiece() {
-        return this.primaryPiece;
-    }
-    
-    public Piece getPieceById(char id) {
-        for (int i = 0; i < pieces.size(); i++) {
-            if (pieces.get(i).getId() == id) {
-                return pieces.get(i);
-            } 
-        }
-        return null;
-    }
 
-    public int getWidth() {
-        return this.width;
-    }
+		for (Position p : primaryPiece.getPositions()) {
+			if (p.equals(exitPosition)) {
+				return true;
+			}
+		}
 
-    public int getHeight() {
-        return this.height;
-    }
+		 for (Position p : primaryPiece.getPositions()) {
+			if (primaryPiece.getOrientation() == Orientation.HORIZONTAL) {
+				if (p.getY() == exitPosition.getY()) {
+					if (exitPosition.getX() == this.width && p.getX() == this.width - primaryPiece.getPositions().size()) {
+						
+						
+						
+						 List<Position> pPositions = primaryPiece.getPositions();
+						 Position rightMostP = pPositions.get(pPositions.size()-1);
+						 if (exitPosition.getX() == this.width && rightMostP.getX() == this.width -1) return true;
+						 Position leftMostP = pPositions.get(0);
+						 if (exitPosition.getX() == -1 && leftMostP.getX() == 0) return true;
 
-    public Position getExitPosition() {
-        return this.exitPosition;
-    }
+					}
+				}
+			} else {
+				 if (p.getX() == exitPosition.getX()) {
+					List<Position> pPositions = primaryPiece.getPositions();
+					Position bottomMostP = pPositions.get(pPositions.size()-1);
+					if (exitPosition.getY() == this.height && bottomMostP.getY() == this.height -1) return true;
+					Position topMostP = pPositions.get(0);
+					if (exitPosition.getY() == -1 && topMostP.getY() == 0) return true;
+				 }
+			}
+		}
+		return false;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Board board = (Board) o;
+	public ArrayList<Move> getValidMoves() {
+		ArrayList<Move> validMoves = new ArrayList<>();
+		for (Piece piece : this.pieces) {
+			List<Position> piecePositions = piece.getPositions();
+			if (piecePositions.isEmpty()) {
+				continue;
+			}
 
-        if (width != board.width || height != board.height) {
-            return false;
-        }
-        if (!Objects.equals(exitPosition, board.exitPosition)) {
-            return false;
-        }
+			for (Direction direction : Direction.values()) {
+				if ((piece.getOrientation() == Orientation.HORIZONTAL && (direction == Direction.UP || direction == Direction.DOWN)) ||
+					(piece.getOrientation() == Orientation.VERTICAL && (direction == Direction.LEFT || direction == Direction.RIGHT))) {
+					continue;
+				}
 
-        if (this.pieces == null && board.pieces == null) {
-            // Keduanya null, dianggap sama
-        } else if (this.pieces == null || board.pieces == null) {
-            return false; // Salah satunya null, yang lain tidak
-        } else {
-            if (this.pieces.size() != board.pieces.size()) {
-                return false; // Jumlah bidak berbeda
-            }
-             if (!new HashSet<>(this.pieces).equals(new HashSet<>(board.pieces))) {
-                 return false;
-             }
-        }
-        return true;
-    }
+				for (int s = 1; ; s++) {
+					Position positionToTest;
 
-    @Override
-    public int hashCode() {
-        Set<Piece> piecesSet = (pieces == null) ? null : new HashSet<>(pieces);
-        return Objects.hash(width, height, piecesSet, exitPosition);
-    }
+					switch (direction) {
+						case UP:
+							Position currentTop = piecePositions.get(0);
+							positionToTest = new Position(currentTop.getX(), currentTop.getY() - s);
+							break;
+						case DOWN:
+							Position currentBottom = piecePositions.get(piecePositions.size() - 1);
+							positionToTest = new Position(currentBottom.getX(), currentBottom.getY() + s);
+							break;
+						case LEFT:
+							Position currentLeft = piecePositions.get(0);
+							positionToTest = new Position(currentLeft.getX() - s, currentLeft.getY());
+							break;
+						case RIGHT:
+							Position currentRight = piecePositions.get(piecePositions.size() - 1);
+							positionToTest = new Position(currentRight.getX() + s, currentRight.getY());
+							break;
+						default:
+							throw new IllegalStateException("Arah tidak dikenal: " + direction);
+					}
+
+					if (positionToTest.getX() < 0 || positionToTest.getX() >= this.width ||
+						positionToTest.getY() < 0 || positionToTest.getY() >= this.height) {
+						break;
+					}
+
+					if (this.grid[positionToTest.getY()][positionToTest.getX()] != null) {
+						
+						break;
+					}
+
+					validMoves.add(new Move(piece.getId(), direction, s));
+				}
+			}
+		}
+		return validMoves;
+	}
+
+	@Override
+	public Board clone() {
+		ArrayList<Piece> piecesCopy = new ArrayList<>();
+		for (Piece p : this.pieces) {
+			piecesCopy.add(p.clone());
+		}
+		Position exitPositionCopy = new Position(this.exitPosition.getX(), this.exitPosition.getY());
+		return new Board(this.width, this.height, piecesCopy, exitPositionCopy);
+	}
+
+	public List<Piece> getPieces() {
+		ArrayList<Piece> piecesCopy = new ArrayList<>();
+		for(Piece p : this.pieces){
+			piecesCopy.add(p.clone());
+		}
+		return piecesCopy;
+	}
+
+	public Piece getPrimaryPiece() {
+		return this.primaryPiece;
+	}
+
+	public Piece getPieceById(char id) {
+		for (Piece p : this.pieces) {
+			if (p.getId() == id) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public Position getExitPosition() {
+		return new Position(this.exitPosition.getX(), this.exitPosition.getY());
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Board board = (Board) o;
+
+		if (width != board.width || height != board.height) {
+			return false;
+		}
+		if (!Objects.equals(exitPosition, board.exitPosition)) {
+			return false;
+		}
+
+		if (this.pieces == null && board.pieces == null) {
+		} else if (this.pieces == null || board.pieces == null) {
+			return false;
+		} else {
+			if (this.pieces.size() != board.pieces.size()) {
+				return false;
+			}
+			 if (!new HashSet<>(this.pieces).equals(new HashSet<>(board.pieces))) {
+				 return false;
+			 }
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		Set<Piece> piecesSet = (this.pieces == null) ? null : new HashSet<>(this.pieces);
+		return Objects.hash(width, height, piecesSet, exitPosition);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		char[][] charGrid = new char[height][width];
+		for(int r=0; r<height; r++) {
+			for(int c=0; c<width; c++) {
+				charGrid[r][c] = '.';
+			}
+		}
+		for(Piece p : this.pieces) {
+			for(Position pos : p.getPositions()) {
+				if(pos.getX() >= 0 && pos.getX() < width && pos.getY() >=0 && pos.getY() < height) {
+					charGrid[pos.getY()][pos.getX()] = p.getId();
+				}
+			}
+		}
+		for(int r=0; r<height; r++) {
+			for(int c=0; c<width; c++) {
+				sb.append(charGrid[r][c]);
+			}
+			if (exitPosition.getY() == r && exitPosition.getX() == width) sb.append('K');
+			if (exitPosition.getY() == r && exitPosition.getX() == -1) sb.insert(0, 'K');
+			sb.append("\n");
+		}
+		if (exitPosition.getY() == -1) {
+			StringBuilder topExitLine = new StringBuilder();
+			for (int c=0; c<width; c++) topExitLine.append(c == exitPosition.getX() ? 'K' : '.');
+			sb.insert(0, topExitLine.toString() + "\n");
+		}
+		if (exitPosition.getY() == height) {
+			StringBuilder bottomExitLine = new StringBuilder();
+			for (int c=0; c<width; c++) bottomExitLine.append(c == exitPosition.getX() ? 'K' : '.');
+			sb.append(bottomExitLine.toString()).append("\n");
+		}
+		return sb.toString();
+	}
 }
